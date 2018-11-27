5b2549a0d5b57bf873a5e5dc36c8b0887cd062a8
Kelsey Wojczak <43016136+kwojczak98@users.noreply.github.com>
2018-11-26 20:35:36 -0600

    Whiteboard

--- mod/time_theme/views/default/page/elements/riverwire.php
+++ mod/time_theme/views/default/page/elements/riverwire.php
@@ -4,7 +4,7 @@
  *
  */
 
-$title = elgg_view_title(elgg_echo('thewire'));
+$title = elgg_view_title(elgg_echo('Whiteboard'));
 $content .= elgg_view_form('thewire/add', array('name' => 'elgg-wire'));
 
 echo elgg_view_module('thewire', $title, $content);
